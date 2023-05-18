9cd512fa0d0b902e78f2639143199ece167cf670
Fatrald <55173900+Fatrald@users.noreply.github.com>
2023-05-18 18:22:09 +0700

    Initial commit

---
+++ app/src/main/java/com/bangkit/ewaste/ui/profile/ProfileFragment.kt
@@ -0,0 +1,52 @@
+package com.bangkit.ewaste.ui.profile
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.bangkit.ewaste.R
+import com.bangkit.ewaste.databinding.FragmentHomeBinding
+import com.bangkit.ewaste.databinding.FragmentProfileBinding
+import com.bangkit.ewaste.ui.home.HomeViewModel
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
+
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textProfile
+        profileViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
