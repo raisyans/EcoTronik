31df6b29fb0d517e78455dc7962090a85f5a147d
Fatrald <55173900+Fatrald@users.noreply.github.com>
2023-05-29 22:21:44 +0700

    init

--- app/src/main/java/com/bangkit/ewaste/ui/registration/RegistrationViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.bangkit.ewaste.ui.registration
-
-import android.content.Context
-import androidx.lifecycle.ViewModel
-
-class RegistrationViewModel(context: Context) : ViewModel() {
-    // TODO: Implement the ViewModel
-}
